bb9dee1db3f7e7b5c4468f326ffa1cd6d0fc4b09
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-11-26 03:13:36 +0330

    course user list complste

--- src/views/apps/ecommerce/useList/index.js
+++ src/views/apps/ecommerce/useList/index.js
@@ -1,102 +1,122 @@
 import React, { useEffect, useState } from "react";
 import TableUserList from "../../tables/CourseUserListTable/TableUserList";
 import {
-    Card,
-    Row,
-    Col,
-    Button,
-    Modal,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    Input,
-    InputGroup,
-    InputGroupText,
-  } from "reactstrap";
+  Card,
+  Row,
+  Col,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Input,
+  InputGroup,
+  InputGroupText,
+} from "reactstrap";
 import { Search } from "react-feather";
 import TableProduct from "../../tables/productListTable/TableProduct";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { getProducts } from "../../../../core/Services/api/getCourseList";
 import { getUserList } from "../../../../core/Services/api/useSection/inedx";
+import { useDebounce } from "use-debounce";
 
 function CourseUserList() {
   const [centeredModal, setCenteredModal] = useState(false);
   const [selectedId, setSelectedId] = useState(null);
-
+  const [searchProduct, setSearchProduct] = useState("");
+  const [debounceProduct] = useDebounce(searchProduct, 500);
+  const [searchUser, setSearchUser] = useState("");
+  const [debounceUser] = useDebounce(searchUser, 500);
 
   const apiParams = {
     PageNumber: 1,
-    RowsOfPage: 1000
+    RowsOfPage: 1000,
+    Query: debounceProduct,
   };
-  const userApiParams ={
+  const userApiParams = {
     PageNumber: 1,
     RowsOfPage: 1000,
-    CourseId:selectedId
-  }
+    CourseId: selectedId,
+    Query: debounceUser,
+  };
   const { data: courses, isLoading } = useQuery({
     queryFn: () => getProducts(apiParams),
     queryKey: ["productsList", apiParams],
-    refetchOnWindowFocus:false
+    refetchOnWindowFocus: false,
   });
-  
-console.log(selectedId)
-  
-const current=courses?.courseDtos
-const { data: users,isLoading:loading } = useQuery({
+
+  const current = courses?.courseDtos;
+  const { data: users, isLoading: loading } = useQuery({
     queryFn: () => getUserList(userApiParams),
     queryKey: ["userCourseList", userApiParams],
-    enabled:!!selectedId,
-    refetchOnWindowFocus:false
+    enabled: !!selectedId,
+    refetchOnWindowFocus: false,
   });
-  console.log("users",users)
   return (
     <div>
       <Row className="d-flex justify-content-between align-items-center">
-      <Col sm='6' >
-          <InputGroup className='input-group-merge'>
+        <Col sm="6">
+          <InputGroup className="input-group-merge">
             <Input
-              className='search-product'
-              placeholder='جستجو...'
+              className="search-product"
+              placeholder="جستجو کاربران..."
+              onChange={(e) => setSearchUser(e.target.value)}
+              value={searchUser}
             />
             <InputGroupText>
-              <Search className='text-muted' size={14} />
+              <Search className="text-muted" size={14} />
             </InputGroupText>
           </InputGroup>
         </Col>
         <Col xl="3" md="4" sm="6">
-        <Button
-          color="relief-primary"
-          style={{ width: "100%" }}
-          onClick={() => setCenteredModal(!centeredModal)}
-        >
-        انتخاب دوره
-        </Button>
-        <Modal
-          isOpen={centeredModal}
-          toggle={() => setCenteredModal(!centeredModal)}
-          className="modal-dialog-centered"
-        >
-          <ModalHeader toggle={() => setCenteredModal(!centeredModal)}></ModalHeader>
+          <Button
+            color="relief-primary"
+            style={{ width: "100%" }}
+            onClick={() => setCenteredModal(!centeredModal)}
+          >
+            انتخاب دوره
+          </Button>
+          <Modal
+            isOpen={centeredModal}
+            toggle={() => setCenteredModal(!centeredModal)}
+            className="modal-dialog-centered"
+          >
+            <ModalHeader
+              toggle={() => setCenteredModal(!centeredModal)}
+            ></ModalHeader>
 
-          <ModalBody>
-          <InputGroup className='input-group-merge'>
-            <Input
-              className='search-product'
-              placeholder='جستجو...'
-            />
-            <InputGroupText>
-              <Search className='text-muted' size={14} />
-            </InputGroupText>
-          </InputGroup>
-           <TableProduct data={current} isLoading={isLoading} selectedId={selectedId} setSelectedId={setSelectedId} centeredModal={centeredModal} setCenteredModal={setCenteredModal}/>
-          </ModalBody>
-        </Modal>
+            <ModalBody>
+              <InputGroup className="input-group-merge">
+                <Input
+                  className="search-product"
+                  placeholder="جستجو دوره..."
+                  onChange={(e) => setSearchProduct(e.target.value)}
+                  value={searchProduct}
+                />
+                <InputGroupText>
+                  <Search className="text-muted" size={14} />
+                </InputGroupText>
+              </InputGroup>
+              <TableProduct
+                data={current}
+                isLoading={isLoading}
+                selectedId={selectedId}
+                setSelectedId={setSelectedId}
+                centeredModal={centeredModal}
+                setCenteredModal={setCenteredModal}
+              />
+            </ModalBody>
+          </Modal>
         </Col>
       </Row>
 
       <Row>
-        <Col style={{marginTop:20}}>
-          <TableUserList data={users} isLoading={loading} selectedId={selectedId}/>
+        <Col style={{ marginTop: 20 }}>
+          <TableUserList
+            data={users}
+            isLoading={loading}
+            selectedId={selectedId}
+          />
         </Col>
       </Row>
     </div>
