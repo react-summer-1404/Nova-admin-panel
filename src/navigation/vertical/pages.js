f71567dac562eee0ae62bb13562554ea5f4bf63c
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-02 02:08:32 +0330

    rate news done

--- src/navigation/vertical/pages.js
+++ src/navigation/vertical/pages.js
@@ -2,14 +2,14 @@ import { Circle, FileText, MessageSquare } from "react-feather";
 
 export default [
   {
-    id: "pages",
-    title: "مدیریت صفحات",
-    icon: <FileText size={20} />,
-    children: [
-      {
+    // id: "pages",
+    // title: "مدیریت صفحات",
+    // icon: <FileText size={20} />,
+    // children: [
+      // {
         id: "blog",
         title: "اخبار و مقالات",
-        icon: <Circle size={12} />,
+        icon: <FileText size={12} />,
         children: [
           {
             id: "blogList",
@@ -17,12 +17,12 @@ export default [
             permissions: ["admin", "editor"],
             navLink: "/pages/blog/list",
           },
-          {
-            id: "blogDetail",
-            title: "جزِیات اخبار و مقالات",
-            permissions: ["admin", "editor"],
-            navLink: "/pages/blog/detail",
-          },
+          // {
+          //   id: "blogDetail",
+          //   title: "جزِیات اخبار و مقالات",
+          //   permissions: ["admin", "editor"],
+          //   navLink: "/pages/blog/detail",
+          // },
           {
             id: "addBlogs",
             title: "افزودن اخبار و مقالات",
@@ -30,13 +30,14 @@ export default [
             navLink: "/pages/blog/edit",
           },
         ],
-      },
-      {
-        id: "comments",
-        title: "کامنت ها",
-        icon: <MessageSquare size={20} />,
-        navLink: "/pages/comments",
-      },
-    ],
+      // },
+      // {
+       
+      // },
+    // ],
   },
+  { id: "comments",
+  title: "کامنت ها",
+  icon: <MessageSquare size={20} />,
+  navLink: "/pages/comments",}
 ];
