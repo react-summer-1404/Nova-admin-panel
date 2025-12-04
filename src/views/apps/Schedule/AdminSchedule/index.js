91c6433896f41f7854a1c4041f0d82d5e0ba5252
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-04 21:47:39 +0330

    routs

---
+++ src/views/apps/Schedule/AdminSchedule/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminSchedule = () => {
+  return (
+    <div>
+      AdminSchedule
+    </div>
+  )
+}
+
+export default AdminSchedule
---
+++ src/views/apps/Schedule/StudentSchedule/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentSchedule = () => {
+  return (
+    <div>
+      StudentSchedule
+    </div>
+  )
+}
+
+export default StudentSchedule
