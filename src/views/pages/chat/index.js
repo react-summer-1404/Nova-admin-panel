c2f33839e4af8e8db16597cc1d44f12922549378
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2025-12-10 23:36:14 +0330

    Update index.js

--- src/views/pages/chat/index.js
+++ src/views/pages/chat/index.js
@@ -2,8 +2,8 @@ import ChatBox from "./ChatBox";
 
 export default function ChatPage() {
     return (
-        <div className="p-6">
-        <h1 className="text-2xl font-bold mb-4">چت</h1>
+        <div className="p-4 container">
+        <h1 className="mb-3">چت ادمین</h1>
         <ChatBox />
         </div>
     );
