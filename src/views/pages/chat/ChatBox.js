12c2730d672ae5e1834c014addefe0cb944a6107
baharvhbii-prog <bahar.vhbii@gmail.com>
2025-12-11 17:07:28 +0330

    .

--- src/views/pages/chat/ChatBox.js
+++ src/views/pages/chat/ChatBox.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 const socket = io("http://localhost:3001");
 
