0e0ade4fc3f1bed5cc0fb70b6aa426ddde09c31a
mobina1383tabari-beep <mobina.1383tabari@gmail.com>
2025-12-11 03:38:01 +0330

    true

--- src/views/pages/chat/MessageList.js
+++ src/views/pages/chat/MessageList.js
@@ -1,26 +1,16 @@
-import { useState } from "react";
-
-export default function MessageInput({ onSend }) {
-    const [text, setText] = useState("");
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSend(text);
-        setText("");
-    };
-
+export default function MessageList({ messages= [] }) {
     return (
-        <form onSubmit={handleSubmit} className="d-flex gap-2 " dir= 'rtl'>
-        <input
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            className="form-control"
-            placeholder="پیام خود را بنویسید..."
-        />
-        <button type="submit" className="btn btn-primary" disabled={!text.trim()}>
-            ارسال
-        </button>
-        </form>
-    );
+        <div className="d-flex flex-column gap-2">
+            {messages.map((msg) => (
+            <div
+                key={msg._id}
+                 className={`d-flex ${msg.user?.role === "ادمین" ? "justify-content-end" : "justify-content-start"}`}
+            >
+                <div className="bg-light border rounded px-3 py-2">
+                <strong>{msg.user?.role}:</strong> {msg.text}
+                </div>
+            </div>
+            ))}
+        </div>
+        );
 }
